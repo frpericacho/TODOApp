a8870cc9e4f21a38c62e227c0b6a25ecb5cc01ab
frpericacho <frpericacho@gmail.com>
2022-04-03 18:28:53 +0200

    table schema added

--- model/schema.js
+++ model/schema.js
@@ -1,8 +1,39 @@
-import { appSchema, tableSchema } from '@nozbe/watermelondb'
+import { appSchema, tableSchema } from "@nozbe/watermelondb";
 
 export default appSchema({
-    version: 1,
-    tables: [
-      // We'll add tableSchemas here later
-    ]
-  })
+  version: 1,
+  tables: [
+    tableSchema({
+      name: "todo_texts",
+      columns: [
+        { name: "title", type: "string" },
+        { name: "text", type: "string" },
+        { name: "is_finish", type: "boolean" },
+      ],
+    }),
+    tableSchema({
+        name: "todo_lists",
+        columns: [
+          { name: "title", type: "string" },
+          { name: "is_finish", type: "boolean" },
+        ],
+      }),
+      tableSchema({
+        name: "items",
+        columns: [
+          { name: "text", type: "string" },
+          { name: "is_finish", type: "boolean" },
+          { name: "todo_list_id", type: "string" },
+        ],
+      }),
+      tableSchema({
+        name: "events",
+        columns: [
+          { name: "created_at", type: "number" },
+          { name: "is_finish", type: "boolean" },
+          { name: "todo_list_id", type: "string", isOptional: true },
+          { name: "todo_text_id", type: "string", isOptional: true },
+        ],
+      }),
+  ],
+});
